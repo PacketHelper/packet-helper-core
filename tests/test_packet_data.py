b0043c692e26c6d5a301d741a10ae0acd883755f
Nex Sabre <NexSabre@users.noreply.github.com>
2021-07-03 19:42:14 +0200

    Add custom packet test to TestPacketData

--- tests/test_packet_data.py
+++ tests/test_packet_data.py
@@ -1,3 +1,8 @@
+from scapy.layers.inet import IP, TCP
+from scapy.layers.inet6 import IPv6
+from scapy.layers.l2 import Ether
+from scapy_helper import get_hex
+
 from packet_helper_core.packet_data import PacketData
 from packet_helper_core.utils.utils import decode_hex
 from tests.utils.example_packets import EXAMPLE_ETHER
@@ -12,3 +17,12 @@ class TestPacketData:
 
         pd = PacketData(raw=str(packet))
         assert "ETH" in pd.header, "Ether header should be found at packet"
+
+    def test_custom_packet_data(self):
+        frame = Ether() / IP() / IPv6() / TCP()
+        packet = decode_hex(get_hex(frame))
+        list_of_expected_packets = ("ETH", "IP", "IPV6", "TCP")
+        list_of_layers_from_packet = [x.layer_name.upper() for x in packet.layers]
+        for expected_packet in list_of_expected_packets:
+            if expected_packet not in list_of_layers_from_packet:
+                raise Exception(f"Missing layer ${expected_packet} in packet. PyShark decode correctly?")
