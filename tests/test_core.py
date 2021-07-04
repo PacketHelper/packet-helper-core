a76b7c8110de3c3dc7524898dfb13559b7bbd034
Nex Sabre <NexSabre@users.noreply.github.com>
2021-07-04 21:48:28 +0200

    Add a Core class and simple test for it

---
+++ tests/test_core.py
@@ -0,0 +1,30 @@
+from scapy.layers.inet import IP, TCP
+from scapy.layers.l2 import Ether
+from scapy_helper import get_hex
+
+from packet_helper_core import PacketDataScapy, PacketData
+from packet_helper_core.core import Core
+
+
+class TestCore:
+    simple_ether_ip_tcp_hex_string = get_hex(Ether() / IP() / TCP())
+
+    def test_core_post_init(self):
+        core_results = Core(TestCore.simple_ether_ip_tcp_hex_string)
+
+        assert isinstance(core_results.hex_string, str), "Should be String"
+        assert isinstance(
+            core_results.scapy_data, PacketDataScapy
+        ), "Should be PacketDataScapy"
+        assert isinstance(core_results.tshark_data, PacketData), "Should be PacketData"
+
+        assert core_results.scapy_data.header == [
+            "ETH",
+            "IP",
+            "TCP",
+        ], "Should be properly decoded"
+        assert core_results.tshark_data.header == [
+            "ETH",
+            "IP",
+            "TCP",
+        ], "Should be properly decoded"
