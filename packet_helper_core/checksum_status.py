99d0b013874bcf9556db4d736ab8af1de2df04b4
Nex Sabre <NexSabre@users.noreply.github.com>
2021-07-17 14:16:37 +0200

    Update checksum_status.py

--- packet_helper_core/checksum_status.py
+++ packet_helper_core/checksum_status.py
@@ -10,5 +10,6 @@ class ChecksumStatus:
     def __call__(self, *args, **kwargs):
         def clean_chksum(element: str):
             return element.replace("0x", "")
-
+        if self.chksum == "" or self.chksum_calculated == "":
+            return
         self.status = clean_chksum(self.chksum) == clean_chksum(self.chksum_calculated)
